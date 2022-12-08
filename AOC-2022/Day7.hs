83d18879daf4a5ef150aa75406b1d9a2a6759f3a
Perigord <90542764+Trouble-Truffle@users.noreply.github.com>
2022-12-08 09:15:42 +0800

    Update Day7.hs

--- AOC-2022/Day7.hs
+++ AOC-2022/Day7.hs
@@ -1,101 +1,90 @@
 {-# LANGUAGE TemplateHaskell #-}
-{-# LANGUAGE LambdaCase #-}
-{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE ViewPatterns #-}
 module Main
   ( main
   ) where
 
-import           Control.Arrow                  (Arrow((&&&), first, second)
-                                                )
+import           Control.Arrow                  ( Arrow((&&&), first, second) )
 import           Control.Lens            hiding ( uncons )
 import           Control.Monad.State
-import qualified Data.Map                      as M
+import           Data.Char
+import qualified Data.Map.Strict               as M
+import           Data.Maybe                     ( fromMaybe )
 import           Data.Tree
 
+type Dir = M.Map String File
 data File
   = Written Int
-  | Directory (M.Map String File)
+  | Directory Dir
   deriving (Show)
 
 data Files = Files
-  { _upper  :: Maybe Files
-  , _dir    :: M.Map String File
-  , _lastCd :: String
+  { _path :: [String]
+  , _tree :: Dir
   }
   deriving Show
 makeLenses ''Files
 
-file :: String -> Lens' Files (Maybe File)
-file s = dir . at s
+recursiveAlterF
+  :: (Functor f) => (Maybe Dir -> f (Maybe Dir)) -> [String] -> Dir -> f Dir
+recursiveAlterF f []       x = fmap (fromMaybe mempty) $ f $ Just x
+recursiveAlterF f (k : ks) x = M.alterF f' k x
+ where
+  f' (Just (Written _)) = error "cd into File"
+  f' (Just (Directory dir)) | null ks   = fmap (fmap Directory) <$> f $ Just dir
+                            | otherwise = Just . Directory <$> recursiveAlterF f ks dir
+  f' Nothing = Nothing <$ f Nothing
 
-getFileSystem :: [String] -> State Files ()
-getFileSystem [] = do
-  lastCd' <- gets _lastCd
-  gets _upper >>= \case
-    Just x -> do
-      prevDir <- gets _dir
-      put x
-      file lastCd' %= Just . maybe (Directory prevDir)
-                                   (\(Directory a) -> Directory (a <> prevDir))
-      getFileSystem []
-    Nothing -> return ()
+curDir :: Lens' Files Dir
+curDir f (Files path' tree') = fmap
+  (Files path')
+  (recursiveAlterF (maybe (Nothing <$ f tree') (fmap Just . f)) path' tree')
 
-getFileSystem ("$ ls"                      : xs) = getFileSystem xs
-getFileSystem (('d' : 'i' : 'r' : ' ' : x) : xs) = do
-  dir %= M.insert x (Directory M.empty)
-  getFileSystem xs
-getFileSystem ("$ cd .." : xs) = do
-  lastCd' <- gets _lastCd
-  gets _upper >>= \case
-    Just x -> do
-      prevDir <- gets _dir
-      put x
-      file lastCd' %= Just . maybe (Directory prevDir)
-                                   (\(Directory a) -> Directory (a <> prevDir))
-    Nothing -> pure ()
-  getFileSystem xs
-getFileSystem (('$' : ' ' : 'c' : 'd' : ' ' : x) : xs) = do
-  state' <- get
-  gets (^. file x) >>= \case
-    Just (Written   _) -> error $ "Attempt to cd into file: " ++ x
-    Just (Directory d) -> put (Files (Just state') d x)
-    Nothing            -> put (Files (Just state') M.empty x)
-  getFileSystem xs
-getFileSystem (x : xs) = do
-  let [size, name] = words x
-  dir %= M.insert name (Written $ read size)
-  getFileSystem xs
+fileSystem :: [String] -> State Files (M.Map String File)
+fileSystem [] = gets (view tree)
+fileSystem ("$ ls" : xs) = fileSystem xs
+fileSystem (('d' : 'i' : 'r' : ' ' : x) : xs) = do
+  curDir %= M.insert x (Directory mempty)
+  fileSystem xs
+fileSystem ("$ cd .." : xs) = do
+  path %= init
+  fileSystem xs
+fileSystem (('$' : ' ' : 'c' : 'd' : ' ' : x) : xs) = do
+  path %= (<> [x])
+  fileSystem xs
+fileSystem ((span isDigit -> (size,name)) : xs) = do
+  curDir %= M.insert (tail name) (Written (read size))
+  fileSystem xs
 
-toTree :: File -> Tree [Int]
-toTree (Written   _ ) = undefined
-toTree (Directory xs) = Node l (map toTree r)
+toTree :: M.Map String File -> Tree [Int]
+toTree xs = Node l (map toTree r)
  where
   (l, r) = partitionFiles $ map snd $ M.toList xs
-  partitionFiles :: [File] -> ([Int], [File])
+  partitionFiles :: [File] -> ([Int], [M.Map String File])
   partitionFiles []                 = ([], [])
   partitionFiles (Written   x : ys) = first (x :) $ partitionFiles ys
-  partitionFiles (Directory x : ys) = second (Directory x :) $ partitionFiles ys
+  partitionFiles (Directory x : ys) = second (x :) $ partitionFiles ys
 
-subTrees :: Tree a -> Tree [a]
-subTrees (Node x []) = Node [x] []
-subTrees (Node x ts) = Node (x : concatMap flatten ts) $ map subTrees ts
+subTrees :: Tree a -> Tree (Tree a)
+subTrees (Node x []) = Node (Node x []) []
+subTrees (Node x ts) = Node (Node x ts) $ map subTrees ts
 
-solution2 :: Tree Int -> Int
-solution2 xs =
-  let requiredSpace = 30000000 - (70000000 - sum (flatten xs))
-  in  minimum $ filter (>= requiredSpace) $ flatten $ sum <$> subTrees xs
+solution2 :: [Int] -> Int
+solution2 [] = 0
+solution2 (x : xs) =
+  let requiredSpace = 30000000 - (70000000 - x) in minimum $ filter (>= requiredSpace) xs
 
 main :: IO ()
 main =
   do
       print
-    .   ((sum . filter (<= 100000) . flatten . fmap sum . subTrees) &&& solution2)
-    .   fmap sum
+    .   ((sum . filter (<= 100000)) &&& solution2)
+    .   flatten
+    .   fmap (sum . fmap sum)
+    .   subTrees
     .   toTree
-    .   Directory
-    .   _dir
-    .   flip execState (Files Nothing M.empty "/")
-    .   getFileSystem
+    .   flip evalState (Files [] mempty)
+    .   fileSystem
     .   tail
     .   lines
     =<< readFile "input.txt"
