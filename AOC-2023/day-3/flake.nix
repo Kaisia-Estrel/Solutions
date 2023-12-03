7befdedebfc5ed5db0b542364a86ac621d0fbf5d
Trouble-Truffle <perigordtruffle7318@gmail.com>
2023-12-03 22:21:59 +0800

    add AOC Day-3

---
+++ AOC-2023/day-3/flake.nix
@@ -0,0 +1,15 @@
+{
+  description = "A very basic flake";
+
+  outputs = {
+    self,
+    nixpkgs,
+  }: let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs {inherit system;};
+  in {
+    devShells.${system}.default = pkgs.mkShell {
+      packages = with pkgs; [kotlin kotlin-language-server detekt gradle ktfmt jetbrains.idea-community];
+    };
+  };
+}
